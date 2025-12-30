60e85e4d3fdee289d6432a3ffd26806245fa0b2f
phemasundar <p.hemasundar@gmail.com>
2025-12-30 16:51:19 -0600

    Added POJO for Pu Credit Spread

---
+++ src/main/java/com/hemasundar/pojos/PutCreditSpread.java
@@ -0,0 +1,34 @@
+package com.hemasundar.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PutCreditSpread {
+    private OptionChainResponse.OptionData shortPut;
+    private OptionChainResponse.OptionData longPut;
+    private double netCredit;
+    private double maxLoss;
+    private double breakEvenPrice;
+    private double breakEvenPercentage;
+    private double returnOnRisk;
+
+    @Override
+    public String toString() {
+        return String.format("--- Valid Put Credit Spread Found ---\n" +
+                "Strategy: Sell %s (Strike %.1f) / Buy %s (Strike %.1f)\n" +
+                "Short Delta: %.3f | Max Profit: $%.2f | Max Loss: $%.2f\n" +
+                "Return on Risk: %.2f%%\n" +
+                "Break Even Price: $%.2f | Break Even Percentage: %.2f%%\n",
+                shortPut.getSymbol(), shortPut.getStrikePrice(),
+                longPut.getSymbol(), longPut.getStrikePrice(),
+                shortPut.getDelta(), netCredit, maxLoss,
+                returnOnRisk,
+                breakEvenPrice, breakEvenPercentage);
+    }
+}
