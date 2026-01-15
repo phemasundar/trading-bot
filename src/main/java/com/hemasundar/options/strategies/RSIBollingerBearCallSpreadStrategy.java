2eaf992f622aae51c6b46375af4a683f6059acf7
phemasundar <p.hemasundar@gmail.com>
2026-01-15 15:49:27 -0600

    Refactor packages

--- src/main/java/com/hemasundar/options/strategies/RSIBollingerBearCallSpreadStrategy.java
+++ src/main/java/com/hemasundar/options/strategies/RSIBollingerBearCallSpreadStrategy.java
@@ -1,19 +1,6 @@
 package com.hemasundar.options.strategies;
 
-import com.hemasundar.options.models.CallCreditSpread;
-import com.hemasundar.options.models.OptionChainResponse;
-import com.hemasundar.options.models.OptionType;
-import com.hemasundar.options.models.OptionsStrategyFilter;
-import com.hemasundar.options.models.TradeSetup;
-import com.hemasundar.technical.BollingerBandsFilter;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.collections4.CollectionUtils;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * RSI & Bollinger Bands Bear Call Spread Strategy.
@@ -28,92 +15,7 @@ import java.util.stream.Collectors;
  * - DTE: 30 days
  */
 @Log4j2
-public class RSIBollingerBearCallSpreadStrategy extends AbstractTradingStrategy {
-
-    @Override
-    protected List<TradeSetup> findValidTrades(OptionChainResponse chain, String expiryDate,
-            OptionsStrategyFilter filter) {
-        // Find Bear Call Spread trades
-        Map<String, List<OptionChainResponse.OptionData>> callMap = chain.getOptionDataForASpecificExpiryDate(
-                OptionType.CALL, expiryDate);
-
-        if (callMap == null || callMap.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        return findBearCallSpreads(callMap, chain.getUnderlyingPrice(), filter);
-    }
-
-    private List<TradeSetup> findBearCallSpreads(Map<String, List<OptionChainResponse.OptionData>> callMap,
-            double currentPrice, OptionsStrategyFilter filter) {
-        List<TradeSetup> spreads = new ArrayList<>();
-
-        List<Double> sortedStrikes = callMap.keySet().stream()
-                .map(Double::parseDouble)
-                .sorted()
-                .collect(Collectors.toList());
-
-        // Find short call at ~30 delta
-        for (int i = 0; i < sortedStrikes.size(); i++) {
-            double shortStrikePrice = sortedStrikes.get(i);
-            List<OptionChainResponse.OptionData> options = callMap.get(String.valueOf(shortStrikePrice));
-            if (CollectionUtils.isEmpty(options))
-                continue;
-
-            OptionChainResponse.OptionData shortCall = options.get(0);
-
-            // Short call should be OTM (strike > current price) and around 30 delta
-            if (shortStrikePrice <= currentPrice)
-                continue;
-            double shortDelta = shortCall.getAbsDelta();
-            if (shortDelta > filter.getMaxDelta() || shortDelta < 0.25)
-                continue; // ~30 delta range
-
-            // Find long call at ~15-20 delta (further OTM)
-            for (int j = i + 1; j < sortedStrikes.size(); j++) {
-                double longStrikePrice = sortedStrikes.get(j);
-                List<OptionChainResponse.OptionData> longOptions = callMap.get(String.valueOf(longStrikePrice));
-                if (CollectionUtils.isEmpty(longOptions))
-                    continue;
-
-                OptionChainResponse.OptionData longCall = longOptions.get(0);
-
-                double longDelta = longCall.getAbsDelta();
-                if (longDelta > 0.22 || longDelta < 0.12)
-                    continue; // ~15-20 delta range
-
-                double netCredit = (shortCall.getBid() - longCall.getAsk()) * 100;
-                if (netCredit <= 0)
-                    continue;
-
-                double strikeWidth = (longStrikePrice - shortStrikePrice) * 100;
-                double maxLoss = strikeWidth - netCredit;
-
-                if (maxLoss > filter.getMaxLossLimit())
-                    continue;
-
-                double requiredProfit = maxLoss * ((double) filter.getMinReturnOnRisk() / 100);
-
-                if (netCredit >= requiredProfit) {
-                    double breakEvenPrice = shortCall.getStrikePrice() + (netCredit / 100);
-                    double breakEvenPercentage = ((breakEvenPrice - currentPrice) / currentPrice) * 100;
-                    double returnOnRisk = (netCredit / maxLoss) * 100;
-
-                    spreads.add(CallCreditSpread.builder()
-                            .shortCall(shortCall)
-                            .longCall(longCall)
-                            .netCredit(netCredit)
-                            .maxLoss(maxLoss)
-                            .breakEvenPrice(breakEvenPrice)
-                            .breakEvenPercentage(breakEvenPercentage)
-                            .returnOnRisk(returnOnRisk)
-                            .build());
-                }
-            }
-        }
-
-        return spreads;
-    }
+public class RSIBollingerBearCallSpreadStrategy extends CallCreditSpreadStrategy {
 
     @Override
     public String getStrategyName() {
--- src/main/java/com/hemasundar/options/strategies/RSIBollingerBullPutSpreadStrategy.java
+++ src/main/java/com/hemasundar/options/strategies/RSIBollingerBullPutSpreadStrategy.java
@@ -2,18 +2,7 @@ package com.hemasundar.options.strategies;
 
 import com.hemasundar.options.models.OptionChainResponse;
 import com.hemasundar.options.models.OptionsStrategyFilter;
-import com.hemasundar.options.models.OptionType;
-import com.hemasundar.options.models.PutCreditSpread;
-import com.hemasundar.options.models.TradeSetup;
-import com.hemasundar.technical.BollingerBandsFilter;
-import org.apache.commons.collections4.CollectionUtils;
 import lombok.extern.log4j.Log4j2;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * RSI & Bollinger Bands Bull Put Spread Strategy.
@@ -28,92 +17,7 @@ import java.util.stream.Collectors;
  * - DTE: 30 days
  */
 @Log4j2
-public class RSIBollingerBullPutSpreadStrategy extends AbstractTradingStrategy {
-
-    @Override
-    protected List<TradeSetup> findValidTrades(OptionChainResponse chain, String expiryDate,
-            OptionsStrategyFilter filter) {
-        // Find Bull Put Spread trades
-        Map<String, List<OptionChainResponse.OptionData>> putMap = chain.getOptionDataForASpecificExpiryDate(
-                OptionType.PUT, expiryDate);
-
-        if (putMap == null || putMap.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        return findBullPutSpreads(putMap, chain.getUnderlyingPrice(), filter);
-    }
-
-    private List<TradeSetup> findBullPutSpreads(Map<String, List<OptionChainResponse.OptionData>> putMap,
-            double currentPrice, OptionsStrategyFilter filter) {
-        List<TradeSetup> spreads = new ArrayList<>();
-
-        List<Double> sortedStrikes = putMap.keySet().stream()
-                .map(Double::parseDouble)
-                .sorted()
-                .toList();
-
-        // Find short put at ~30 delta
-        for (int i = 0; i < sortedStrikes.size(); i++) {
-            double shortStrikePrice = sortedStrikes.get(i);
-            List<OptionChainResponse.OptionData> options = putMap.get(String.valueOf(shortStrikePrice));
-            if (CollectionUtils.isEmpty(options))
-                continue;
-
-            OptionChainResponse.OptionData shortPut = options.get(0);
-
-            // Short put should be OTM and around 30 delta
-            if (shortStrikePrice >= currentPrice)
-                continue;
-            double shortDelta = shortPut.getAbsDelta();
-            if (shortDelta > filter.getMaxDelta() || shortDelta < 0.25)
-                continue; // ~30 delta range
-
-            // Find long put at ~15-20 delta (further OTM)
-            for (int j = 0; j < i; j++) {
-                double longStrikePrice = sortedStrikes.get(j);
-                List<OptionChainResponse.OptionData> longOptions = putMap.get(String.valueOf(longStrikePrice));
-                if (CollectionUtils.isEmpty(longOptions))
-                    continue;
-
-                OptionChainResponse.OptionData longPut = longOptions.get(0);
-
-                double longDelta = longPut.getAbsDelta();
-                if (longDelta > 0.22 || longDelta < 0.12)
-                    continue; // ~15-20 delta range
-
-                double netCredit = (shortPut.getBid() - longPut.getAsk()) * 100;
-                if (netCredit <= 0)
-                    continue;
-
-                double strikeWidth = (shortStrikePrice - longStrikePrice) * 100;
-                double maxLoss = strikeWidth - netCredit;
-
-                if (maxLoss > filter.getMaxLossLimit())
-                    continue;
-
-                double requiredProfit = maxLoss * ((double) filter.getMinReturnOnRisk() / 100);
-
-                if (netCredit >= requiredProfit) {
-                    double breakEvenPrice = shortPut.getStrikePrice() - (netCredit / 100);
-                    double breakEvenPercentage = ((currentPrice - breakEvenPrice) / currentPrice) * 100;
-                    double returnOnRisk = (netCredit / maxLoss) * 100;
-
-                    spreads.add(PutCreditSpread.builder()
-                            .shortPut(shortPut)
-                            .longPut(longPut)
-                            .netCredit(netCredit)
-                            .maxLoss(maxLoss)
-                            .breakEvenPrice(breakEvenPrice)
-                            .breakEvenPercentage(breakEvenPercentage)
-                            .returnOnRisk(returnOnRisk)
-                            .build());
-                }
-            }
-        }
-
-        return spreads;
-    }
+public class RSIBollingerBullPutSpreadStrategy extends PutCreditSpreadStrategy {
 
     @Override
     public String getStrategyName() {
