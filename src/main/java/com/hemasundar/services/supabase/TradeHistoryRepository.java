7f8ea057c361ae4aa348a4e2d561d89b3e702d31
phemasundar <p.hemasundar@gmail.com>
2026-08-17 21:50:25 -0500

    Updated default time zone to CDT

--- src/main/java/com/hemasundar/services/supabase/TradeHistoryRepository.java
+++ src/main/java/com/hemasundar/services/supabase/TradeHistoryRepository.java
@@ -11,6 +11,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
